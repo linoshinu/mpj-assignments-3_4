2270c1f386056eaf383384bb951c19d874f47b1e
linoshinu <linoshinu@gmail.com>
2026-04-08 16:31:48 +0530

    Update Hillstations.java

--- Hillstations.java
+++ Hillstations.java
@@ -1,51 +1,57 @@
-class Hillstations {
-    void famousfood() {
-        System.out.println("General hill station food");
-    }
-    void famousfor() {
-        System.out.println("General hill station attraction");
+class HillStation {
+    void location() {
+        System.out.println("Hill station location");
     }
 
+    void climate() {
+        System.out.println("Cool climate");
+    }
+}
+
+class Shimla extends HillStation {
+    void location() {
+        System.out.println("Shimla is in Himachal Pradesh");
+    }
+
+    void climate() {
+        System.out.println("Shimla has cold winters");
+    }
+}
+
+class Munnar extends HillStation {
+    void location() {
+        System.out.println("Munnar is in Kerala");
+    }
+
+    void climate() {
+        System.out.println("Munnar has pleasant climate");
+    }
+}
+
+class Mussoorie extends HillStation {
+    void location() {
+        System.out.println("Mussoorie is in Uttarakhand");
+    }
+
+    void climate() {
+        System.out.println("Mussoorie is known as Queen of Hills");
+    }
+}
+
+public class TestHill {
     public static void main(String[] args) {
-        Hillstations h;
+        HillStation h;
 
-        h = new Manali();
-        h.famousfood();
-        h.famousfor();
+        h = new Shimla();
+        h.location();
+        h.climate();
 
-        h = new Darjeeling();
-        h.famousfood();
-        h.famousfor();
+        h = new Munnar();
+        h.location();
+        h.climate();
 
-        h = new Ooty();
-        h.famousfood();
-        h.famousfor();
-    }
-}
-
-class Manali extends Hillstations {
-    void famousfood() {
-        System.out.println("Manali: Siddu and Babru");
-    }
-    void famousfor() {
-        System.out.println("Manali: Snow and adventure sports");
-    }
-}
-
-class Darjeeling extends Hillstations {
-    void famousfood() {
-        System.out.println("Darjeeling: Momos and Thukpa");
-    }
-    void famousfor() {
-        System.out.println("Darjeeling: Tea gardens and sunrise");
-    }
-}
-
-class Ooty extends Hillstations {
-    void famousfood() {
-        System.out.println("Ooty: Varkey and homemade chocolate");
-    }
-    void famousfor() {
-        System.out.println("Ooty: Nilgiri hills and toy train");
+        h = new Mussoorie();
+        h.location();
+        h.climate();
     }
 }
